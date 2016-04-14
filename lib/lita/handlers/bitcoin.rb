24063314fa5841c578ea33e72689c644cc08ee71
lebrauncom <lebraun.com@gmail.com>
2016-04-14 18:24:50 +1000

    add command: true

--- lib/lita/handlers/bitcoin.rb
+++ lib/lita/handlers/bitcoin.rb
@@ -3,7 +3,7 @@ module Lita
     class Bitcoin < Handler
       URL = 'https://blockchain.info/ticker'
 
-      route(/bitcoin|btc/i, :reply, help: { "bitcoin" => "Returns the current BTC/USD exchange rate." })
+      route(/bitcoin|btc/i, :reply, command: true, help: { "bitcoin" => "Returns the current BTC/USD exchange rate." })
 
       def reply(response)
         response.reply "1 BTC is currently worth #{rate}"
